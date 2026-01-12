6162a33969068cae8f5b01791823c1a15dd9665d
Charlie Lamb <charlielamb20@icloud.com>
2026-01-12 09:17:43 +0000

    chore: refactor logo

--- packages/ui/src/components/brand/logo.tsx
+++ packages/ui/src/components/brand/logo.tsx
@@ -1,7 +1,13 @@
+import { Link } from "@ferix/i18n/navigation";
+import { CirclesThreeIcon } from "@phosphor-icons/react";
+
 export function Logo() {
   return (
-    <span className="whitespace-nowrap font-black text-lg uppercase italic">
-      Ferix AI
-    </span>
+    <Link className="group/logo flex items-center gap-1" href="/">
+      <CirclesThreeIcon weight="bold" />
+      <span className="mb-0.5 whitespace-nowrap font-bold italic tracking-tigh group-hover/logo:underline">
+        ferix
+      </span>
+    </Link>
   );
 }
